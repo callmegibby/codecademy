9122059171e5a2e507493db4e9b4d062d92e76c8
callmegibby <71517445+callmegibby@users.noreply.github.com>
2020-09-23 16:33:46 -0400

    new

--- Python3/First_game.py
+++ Python3/First_game.py
@@ -1,5 +1,5 @@
 print ("Welcome to my first game!")
-name = input("What is your name? ")
+name = input("What is your name?")
 age = int(input("What is your age? "))
 
 health = 10
@@ -14,4 +14,18 @@ if wants_to_play == "yes":
     
     left_or_right = input("First choice... Left or Right (left/right)?")
 if left_or_right == "left":
-    ans = ("Nice, you follow the path and reach a lake... Do you wim across or go around (across/around)?")
+    print ("Nice, you follow the path and reach a lake... Do you swim across or go around (left/right)?")
+if left_or_right == "right":
+    print ("You fell of a cliff")
+
+    swim_or_around = input("Aweomse, do you want to swim across the lake or go around? (swim/around")
+if swim_or_around == "swim":
+    print ("You made it across but lost 5 health to sharks. You see a house and a river. Do you go inside the house or swim down river? (house/river")
+if swim_or_around == "around":
+    print ("You made it safley to the house, Congrats!")
+
+    house_or_river = input("Final attempt, choose wisley...(house/river")
+if house_or_river == "house":
+    print ("You made it safley to the house, Congrats!")
+if house_or_river == "river":
+    print ("You have drowned")
