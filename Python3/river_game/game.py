e4df1022f4a0e4e476b738b4a30deba059b0a07f
callmegibby <71517445+callmegibby@users.noreply.github.com>
2020-09-23 18:42:19 -0400

    final commit for game with clean exit and else, elif

--- Python3/river_game/game.py
+++ Python3/river_game/game.py
@@ -22,6 +22,7 @@ if b > a:
 
 '''
 
+import sys
 
 # Introduction
 print("Welcome to my first game!")
@@ -39,60 +40,79 @@ health = 10
 if age >= 18:
     print("You are old enough to play!")
     wants_to_play = input("Do you want to play?").lower()
+    # elif ...
     # if else ... bye
-elif: age <=18
-    print("you are too young")
 
-else:
-    (SystemExit)
+elif age <= 18:
+    print("you are too young")
+    print("End Game")
+    sys.exit
 
 # if yes ..
 if wants_to_play == "yes":
     print("You are staring with", health, "health")
     print("Let's play!")
-    left_or_right = input("First choice... Left or Right (left/right)?")
+    left_or_right = input("First choice... Left or Right (left/right)? ")
     # if else ... bye
 else:
-    (SystemExit)
+    print("End Game")
+    sys.exit
 
 # if left ...
 if left_or_right == "left":
     print(
-        "Nice, you follow the path and reach a lake... Do you swim across or go around (left/right)?"
+        "Nice, you follow the path and reach a lake... Do you swim across or go around (left/right)? "
     )
     # collecting input
     swim_or_around = input(
-        "Awesome, do you want to swim across the lake or go around? (swim/around)"
+        "Awesome, do you want to swim across the lake or go around? (swim/around) "
     )
-
+else:
+    print("End Game")
+    sys.exit
 # if right ...
 
 if left_or_right == "right":
     print("You fell of a cliff")
     # collecting input
     swim_or_around = input(
-        "Awesome, do you want to swim across the lake or go around? (swim/around)"
+        "UH HA HA, do you want to swim across the lake or go around? (swim/around) "
     )
+else:
+    print("End Game")
+    sys.exit
 
 # if swim ...
 if swim_or_around == "swim":
     print(
-        "You made it across but lost 5 health to sharks. You see a house and a river. Do you go inside the house or swim down river? (house/river)"
+        "You made it across but lost 5 health to sharks. You see a house and a river. Do you go inside the house or swim down river? (house/river) "
     )
     # collecting input
-    house_or_river = input("Final attempt, choose wisley...(house/river)")
+    house_or_river = input("Final attempt, choose wisley...(house/river) ")
+else:
+    print("End Game")
+    sys.exit
 
 # if around ...
 if swim_or_around == "around":
     print("You made it safely to the house, Congrats!")
-    house_or_river = input("Final attempt, choose wisley...(house/river)")
+    house_or_river = input("Final attempt, choose wisley...(house/river) ")
+else:
+    print("End Game")
+    sys.exit
 
 # if house ...
 if house_or_river == "house":
     print("You made it safely to the house, Congrats!")
     # end game
+else:
+    print("End Game")
+    sys.exit
 
 # if river ...
 if house_or_river == "river":
     print("You have drowned")
     # end game
+else:
+    print("End Game")
+    sys.exit
--- python2/printing_string.py
+++ python2/printing_string.py
@@ -1,9 +1,8 @@
 import string
-
 """Tell Python to print "Monty Python"
 to the console on line 4!"""
 
-print ("Your string goes here")
+print("Your string goes here")
 
 # Write your code below, starting on line 3!
 
